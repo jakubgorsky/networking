68920d94e83b04075915a342daa24f22a8c6f936
jakub_gorsky <kuba.gorsky@gmail.com>
2022-08-02 13:28:21 +0200

    Created basic structure for a TCP server

---
+++ headers/SERVER.h
@@ -0,0 +1,20 @@
+//
+// Created by Jakub Górski on 02/08/2022.
+//
+
+#ifndef NETWORKING_SERVER_H
+#define NETWORKING_SERVER_H
+
+#pragma comment(lib, "Ws_32.lib")
+
+#include <sdkddkver.h>
+#include <conio.h>
+#include <cstdio>
+
+#include <winsock2.h>
+#include <Windows.h>
+#include <iostream>
+
+#define SCK_VERSION2 0x0202
+
+#endif //NETWORKING_SERVER_H
--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
+#include <iostream>
+
 int main(int argc, char *argv[]) {
 
     // dis a test change to see if push works
+    std::cout<<"1";
     return 0;
 }
